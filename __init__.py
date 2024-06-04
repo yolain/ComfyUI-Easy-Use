38851372e15e03eed513238d0af31c292b32c040
yolain <yolain@163.com>
2024-06-04 23:32:31 +0800

    Upgrade version 1.1.8 to comfyregistry

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 __version__ = "1.1.8"
 
 import os
-import glob
 import folder_paths
 import importlib
 from pathlib import Path
@@ -42,35 +41,6 @@ if os.path.exists(styles_path):
 else:
     os.mkdir(styles_path)
     os.mkdir(samples_path)
-
-#合并autocomplete覆盖到pyssss包
-pyssss_path = os.path.join(comfy_path, "custom_nodes", "ComfyUI-Custom-Scripts", "user")
-combine_folder = os.path.join(cwd_path, "autocomplete")
-if os.path.exists(combine_folder):
-    pass
-else:
-    os.mkdir(combine_folder)
-if os.path.exists(pyssss_path):
-    output_file = os.path.join(pyssss_path, "autocomplete.txt")
-    # 遍历 combine 目录下的所有 txt 文件，读取内容并合并
-    merged_content = ''
-    for file_path in glob.glob(os.path.join(combine_folder, '*.txt')):
-        with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
-            try:
-                file_content = file.read()
-                merged_content += file_content + '\n'
-            except UnicodeDecodeError:
-                pass
-    # 备份之前的autocomplete
-    # bak_file = os.path.join(pyssss_path, "autocomplete.txt.bak")
-    # if os.path.exists(bak_file):
-    #     pass
-    # elif os.path.exists(output_file):
-    #     shutil.copy(output_file, bak_file)
-    if merged_content != '':
-        # 将合并的内容写入目标文件 autocomplete.txt，并指定编码为 utf-8
-        with open(output_file, 'w', encoding='utf-8') as target_file:
-            target_file.write(merged_content)
 
 # ComfyUI-Easy-PS相关 (需要把模型预览图暴露给PS读取，此处借鉴了 AIGODLIKE-ComfyUI-Studio 的部分代码)
 from .py.libs.add_resources import add_static_resource
