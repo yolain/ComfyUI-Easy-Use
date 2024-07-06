9f8ad7763d7abe8468bc1d2ee89f54bc8757e53d
yolain <yolain@163.com>
2024-07-07 00:22:20 +0800

    Add ComfyUI-aki python path to install.bat

--- install.bat
+++ install.bat
@@ -2,13 +2,19 @@
 
 set "requirements_txt=%~dp0\requirements.txt"
 set "python_exec=..\..\..\python_embeded\python.exe"
+set "aki_python_exec=..\..\python\python.exe"
 
 echo Installing EasyUse Requirements...
 
 if exist "%python_exec%" (
     echo Installing with ComfyUI Portable
     "%python_exec%" -s -m pip install -r "%requirements_txt%"
-) else (
+)^
+else if exist "%aki_python_exec%" (
+    echo Installing with ComfyUI Aki
+    "%python_exec%" -s -m pip install -r "%requirements_txt%" 
+)^
+else (
     echo Installing with system Python
     pip install -r "%requirements_txt%"
 )
