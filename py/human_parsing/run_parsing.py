5ec744927b4a16d364a92fe570e7dddf2c6671d8
t00350320 <1040104807@qq.com>
2025-01-10 16:04:31 +0800

    HumanParsing add 'CUDAExecutionProvider'

--- py/human_parsing/run_parsing.py
+++ py/human_parsing/run_parsing.py
@@ -19,7 +19,7 @@ class HumanParsing:
             session_options.execution_mode = ort.ExecutionMode.ORT_SEQUENTIAL
             # session_options.add_session_config_entry('gpu_id', str(gpu_id))
             self.session = ort.InferenceSession(self.model_path, sess_options=session_options,
-                                                providers=['CPUExecutionProvider'])
+                                                providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
 
         parsed_image, mask = onnx_inference(self.session, input_image, mask_components)
         return parsed_image, mask
