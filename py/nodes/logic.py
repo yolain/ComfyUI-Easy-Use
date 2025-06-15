f94a993e34443e5df3cf3da55548299359de719d
Thomas Ward <teward@thomas-ward.net>
2025-06-15 17:55:52 -0400

    Update logic.py: properly handle overwrite mode
    In low-level `OPEN` logic at the system, there are two modes of opening files for writing: `WRITE` which clobbers existing file data, and `APPEND` which allows appending of data.
    
    In the current code, using `if not overwrite: pass` does nothing to define if you're actually appending or overwriting the file in your selection, and instead you should define the file open mode based on analysis of whether you have `overwrite` set to True or not.
    
    This code patch does this.
    
    (discovered as a result of helping someone via the ComfyUI discord)

--- py/nodes/logic.py
+++ py/nodes/logic.py
@@ -1607,8 +1607,10 @@ class saveText:
         if not os.path.exists(output_file_path):
             os.makedirs(output_file_path)
 
-        if not overwrite:
-            pass
+        if overwrite:
+            file_mode = "w"
+        else:
+            file_mode = "a"
 
         log_node_info("Save Text", f"Saving to {filepath}")
 
@@ -1617,13 +1619,13 @@ class saveText:
             for i in text.split("\n"):
                 text_list.append(i.strip())
 
-            with open(filepath, "w", newline="", encoding='utf-8') as csv_file:
+            with open(filepath, file_mode, newline="", encoding='utf-8') as csv_file:
                 csv_writer = csv.writer(csv_file)
                 # Write each line as a separate row in the CSV file
                 for line in text_list:
                     csv_writer.writerow([line])
         else:
-            with open(filepath, "w", newline="", encoding='utf-8') as text_file:
+            with open(filepath, file_mode, newline="", encoding='utf-8') as text_file:
                 for line in text:
                     text_file.write(line)
 
